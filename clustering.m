d0c5de3c265a08638cc2d694c81ba69041e3776f
Tommy <tomrom95@gmail.com>
2017-03-28 15:24:56 -0400

    dendrogram

--- clustering.m
+++ clustering.m
@@ -17,16 +17,23 @@ expression = [colMean, expression(:,3:end)];
 
 times = [0,15, 30, 45, 60, 120];
 
+numClusters = 12;
+
 %% Hierarchical clustering
-% Hierarchical luster with 16 clusters
+% Hierarchical cluster 
 corrDist = pdist(expression, 'corr');
 clusterTree = linkage(corrDist, 'complete');
-clusters = cluster(clusterTree, 'maxclust', 16);
+clusters = cluster(clusterTree, 'maxclust', numClusters);
+
+figure(1)
+dendrogram(clusterTree,0)
+title('Dendrogram for Hierarchical Clustering');
+drawnow;
 
 % Plot time series of clusters
-figure(1)
-for c = 1:16
-    subplot(4,4,c);
+figure(2)
+for c = 1:numClusters
+    subplot(numClusters/4,4,c);
     plot(times,expression((clusters == c),:)');
     axis tight
 end
@@ -35,21 +42,22 @@ suptitle('Hierarchical Clustering of Profiles');
 
 %% K Means
 % K-means clustering with 16 clusters
-[cidx, ctrs] = kmeans(expression, 16,...
+[cidx, ctrs] = kmeans(expression, numClusters,...
     'dist','corr',...
     'rep',5,...
     'disp','final');
-figure(2)
-for c = 1:16
-    subplot(4,4,c);
+figure(3)
+for c = 1:numClusters
+    subplot(numClusters/4,4,c);
     plot(times,expression((cidx == c),:)');
     axis tight
 end
 drawnow;
 suptitle('K-Means Clustering of Profiles');
 
+%% Clustergrams
 % Display clustergram, one for each cluster
-for c = 1:16
+for c = 1:numClusters
     cgram = clustergram(expression((cidx == c),:),'RowLabels',genes((cidx == c)),...
     'ColumnLabels',times, 'Cluster', 'column',...
     'Standardize', 'row');
